70f4216b4d2b7961e0c5d9bda5b38825739a734c
David Baker <dbaker@gmail.com>
2018-03-20 23:10:11 -0400

    Updated default MVC app with Stripe Support

--- StripeTest/Controllers/HomeController.cs
+++ StripeTest/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Stripe;
 using StripeTest.Models;
 
 namespace StripeTest.Controllers
@@ -33,5 +34,41 @@ namespace StripeTest.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
-    }
+
+		public IActionResult Charge(string stripeEmail, string stripeToken)
+		{
+			var customers = new StripeCustomerService();
+			var charges = new StripeChargeService();
+
+			var customer = customers.Create(new StripeCustomerCreateOptions
+			{
+				Email = stripeEmail,
+				SourceToken = stripeToken
+			});
+
+			var charge = charges.Create(new StripeChargeCreateOptions
+			{
+				Amount = 500,
+				Description = "Sample Charge",
+				Currency = "usd",
+				CustomerId = customer.Id
+			});
+
+			return View();
+		}
+
+		//public IActionResult CreateCustomer(string stripeEmail, string stripeToken)
+		//{
+		//	var customers = new StripeCustomerService();
+
+		//	var customer = customers.Create(new StripeCustomerCreateOptions
+		//	{
+		//		Email = stripeEmail,
+		//		SourceToken = stripeToken
+		//	});
+
+		//	return View();
+		//}
+
+	}
 }
--- StripeTest/Startup.cs
+++ StripeTest/Startup.cs
@@ -1,15 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Stripe;
 
 namespace StripeTest
 {
-    public class Startup
+	public class Startup
     {
         public Startup(IConfiguration configuration)
         {
@@ -21,13 +18,16 @@ namespace StripeTest
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+			services.Configure<StripeSettings>(Configuration.GetSection("Stripe"));
             services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
+			StripeConfiguration.SetApiKey(Configuration.GetSection("Stripe")["SecretKey"]);
+
+			if (env.IsDevelopment())
             {
                 app.UseBrowserLink();
                 app.UseDeveloperExceptionPage();
